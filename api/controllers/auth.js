26e08acc56b31b98ed7d46bd670c6be70ce4362e
Anton Zavalniuk <zavalniuk.dev@gmail.com>
2022-07-04 11:45:32 +0200

    connect jwt

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 import User from "../models/User.js";
 import {createError} from "../utils/error.js";
@@ -7,19 +8,20 @@ export const login = async (req, res, next) => {
   try {
     const user = await User.findOne({name: req.body.name})
 
-    if(!user) {
+    if (!user) {
       return next(createError(404, "User with this name not found!"))
     }
 
     const isPasswordMatch = await bcrypt.compare(req.body.password, user.password)
 
-    if(!isPasswordMatch) {
+    if (!isPasswordMatch) {
       return next(createError(400, "Wrong password!"))
     }
 
+    const token = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.JWT_SECRET_KEY)
     const {password, isAdmin, ...restUser} = user._doc
 
-    res.status(201).json(restUser)
+    res.cookie("access_token", token, {httpOnly: true}).status(201).json(restUser)
   } catch (error) {
     next(error)
   }
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
+import cookieParser from "cookie-parser"
 
 import authRoute from "./routes/auth.js"
 import hotelsRoute from "./routes/hotels.js"
@@ -28,6 +29,7 @@ mongoose.connection.on('connected', () => {
 });
 
 // middlewares
+app.use(cookieParser())
 app.use(express.json())
 
 app.use("/api/auth", authRoute)
